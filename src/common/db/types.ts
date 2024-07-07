63e6fb52c37c3f85f57ac202d2a16151d2d32151
Kei Fujikawa <kei@Keis-MacBook-Pro.local>
2024-07-07 17:48:15 +0900

    :wrench: Ignore kysely generated file

--- src/common/db/types.ts
+++ src/common/db/types.ts
@@ -1,34 +1,34 @@
-import type { ColumnType } from "kysely"
+import type { ColumnType } from "kysely";
 
 export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
   ? ColumnType<S, I | undefined, U>
-  : ColumnType<T, T | undefined, T>
+  : ColumnType<T, T | undefined, T>;
 
-export type Timestamp = ColumnType<Date, Date | string, Date | string>
+export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
 export interface Account {
-  createdAt: Generated<Timestamp>
-  id: Generated<string>
-  name: string
-  updatedAt: Generated<Timestamp>
+  createdAt: Generated<Timestamp>;
+  id: Generated<string>;
+  name: string;
+  updatedAt: Generated<Timestamp>;
 }
 
 export interface AccountOrganization {
-  accountId: string
-  createdAt: Generated<Timestamp>
-  organizationId: string
-  updatedAt: Generated<Timestamp>
+  accountId: string;
+  createdAt: Generated<Timestamp>;
+  organizationId: string;
+  updatedAt: Generated<Timestamp>;
 }
 
 export interface Organization {
-  createdAt: Generated<Timestamp>
-  id: Generated<string>
-  name: string
-  updatedAt: Generated<Timestamp>
+  createdAt: Generated<Timestamp>;
+  id: Generated<string>;
+  name: string;
+  updatedAt: Generated<Timestamp>;
 }
 
 export interface DB {
-  account: Account
-  accountOrganization: AccountOrganization
-  organization: Organization
+  account: Account;
+  accountOrganization: AccountOrganization;
+  organization: Organization;
 }
