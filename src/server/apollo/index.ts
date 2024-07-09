69e4746298fa1f71c1710d20de100cd8c7eb9480
Kei Fujikawa <kei@Keis-MacBook-Pro.local>
2024-07-09 18:55:48 +0900

    :fire: Remove

--- src/server/apollo/index.ts
+++ src/server/apollo/index.ts
@@ -4,14 +4,12 @@ import fastifyApollo, {
 } from "@as-integrations/fastify"
 import type { FastifyInstance } from "fastify"
 import { type GraphQLContext, initGraphQLContext } from "./context.js"
-import { graphQLErrorFormatter } from "./error.js"
 import { requestLogPlugin } from "./plugin/requestLogPlugin.js"
 import { graphQLSchema } from "./schema.js"
 
 export const initApolloServer = async (fastify: FastifyInstance) => {
   const apollo = new ApolloServer<GraphQLContext>({
     schema: graphQLSchema,
-    formatError: graphQLErrorFormatter,
     plugins: [fastifyApolloDrainPlugin(fastify), requestLogPlugin()],
   })
 
